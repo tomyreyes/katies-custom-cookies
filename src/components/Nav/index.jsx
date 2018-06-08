ca1c22ba6f09ce4b5854bbd94ce241b78416483e
Tomy <tomyleereyes@gmail.com>
2018-06-08 12:25:05 -0700

    testing responsiveness

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import { Menu } from 'antd'
+
 
 class Nav extends Component {
   render(){
-    return(
-      <div>
-        Navigation
-        </div>
-    )
+    return <div>
+        <Menu>
+          <Menu.Item>Nav</Menu.Item>
+          <Menu.Item>Nav</Menu.Item>
+          <Menu.Item>Nav</Menu.Item>
+        <Menu.Item>Nav</Menu.Item>
+        </Menu>
+      </div>
   }
 }
 export default Nav
