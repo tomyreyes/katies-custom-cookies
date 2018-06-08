08eefe28ee3bc2815058a8feb68d823095526417
Tomy <tomyleereyes@gmail.com>
2018-06-07 21:25:51 -0700

    committing to antd for ui library

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { About, Carousel, Footer, Insta, Nav } from './components'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Nav/>
-        <Carousel/>
-        <About/>
-        <Insta/>
-        <Footer/>
-        </div>
-    );
+        <Nav />
+        <Carousel />
+        <About />
+        <Insta />
+        <Footer />
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
