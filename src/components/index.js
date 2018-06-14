b6cac8baf387baf3c05b4be78c40649a683bdb54
Tomy <tomyleereyes@gmail.com>
2018-06-13 22:29:07 -0700

    stack overhaul, going with Vue

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-
-import About from '../components/About'
-import CarouselContainer from '../components/CarouselContainer'
-import Footer from '../components/Footer'
-import Insta from '../components/Insta'
-import Nav from '../components/Nav'
-
-
-export { About, CarouselContainer, Footer, Insta, Nav}
