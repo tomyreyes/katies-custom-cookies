f1c7c5091d6c4bd9179bd3ed330a4a0c0240e842
Tomy <tomyleereyes@gmail.com>
2018-08-20 19:18:55 -0700

    enhancing form validation with onBlur

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,14 +5,14 @@ export default class Contact extends Component {
 	constructor() {
 		super()
 		this.state = {
-			name: '',
+			fullName: '',
 			email: '',
 			phone: '',
 			message: '',
 			nameError: false,
 			emailError: false,
 			messageError: false,
-			formSuccess: null,
+			disableSubmit: true,
 		}
 	}
 
@@ -21,51 +21,61 @@ export default class Contact extends Component {
 		this.setState({ [name]: value })
 	}
 	handleSubmit = () => {
-		const { name, email, message, phone } = this.state
-		let error = false
+		const { fullName, email, phone, message } = this.state
+		axios.post('http://localhost:8080/send', {
+			fullName, email, phone, message
+		})
+	}
+	checkError = (e) => {
+		const { fullName, email, message } = this.state
+		const { name } = e.target
 		let emailValidation = email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
-		if (name === '') {
-			error = true
-			this.setState({ nameError: true })
-		} else {
-			this.setState({ nameError: false })
+		let error = false
+		if (name === 'fullName') {
+			if (fullName === '') {
+				this.setState({ nameError: true })
+				error = true
+			} else {
+				this.setState({ nameError: false })
+			}
 		}
-		if (email === '' || emailValidation === null) {
-			error = true
-			this.setState({ emailError: true })
-		} else {
-			this.setState({ emailError: false })
+		if (name === 'email') {
+			if (email === '' || emailValidation === null) {
+				this.setState({ emailError: true })
+				error = true
+			} else {
+				this.setState({ emailError: false })
+			}
 		}
-		if (message === '') {
-			error = true
-			this.setState({ messageError: true })
-		} else {
-			this.setState({ messageError: false })
+		if (name === 'message') {
+			if (message === '') {
+				this.setState({ messageError: true })
+				error = true
+			} else {
+				this.setState({ messageError: false })
+			}
 		}
 		if (error) {
-			this.setState({ formSuccess: false })
+			this.setState({ disableSubmit: true })
 		} else {
-			axios.post('http://localhost:8080/send', {
-				name, email, phone, message
-			})
-			this.setState({ formSuccess: true })
+			this.setState({ disableSubmit: false })
 		}
 	}
 
 	render() {
-		const { name, email, phone, message, nameError, emailError, messageError, formSuccess } = this.state
+		const { fullName, email, phone, message, nameError, emailError, messageError, disableSubmit } = this.state
 		return (
-			<Modal trigger={<Menu.Item name='contact' />}>
+			<Modal trigger={< Menu.Item name='contact' />} >
 				<Modal.Header>Contact Me</Modal.Header>
 				<Modal.Content>
 					<Form>
 						<Form.Field error={nameError}>
 							<label>Full Name</label>
-							<input value={name} name="name" onChange={(e) => this.handleUserInput(e)} />
+							<input value={fullName} name="fullName" onBlur={(e) => this.checkError(e)} onChange={(e) => this.handleUserInput(e)} />
 						</Form.Field>
 						<Form.Field error={emailError}>
 							<label>Email</label>
-							<input value={email} name="email" onChange={(e) => this.handleUserInput(e)} />
+							<input value={email} name="email" onBlur={(e) => this.checkError(e)} onChange={(e) => this.handleUserInput(e)} />
 						</Form.Field>
 						<Form.Field>
 							<label>Phone Number</label>
@@ -73,14 +83,14 @@ export default class Contact extends Component {
 						</Form.Field>
 						<Form.Field error={messageError}>
 							<label>Message</label>
-							<textarea value={message} name="message" onChange={(e) => this.handleUserInput(e)} />
+							<textarea value={message} name="message" onBlur={(e) => this.checkError(e)} onChange={(e) => this.handleUserInput(e)} />
 						</Form.Field>
-						<Button color='blue' onClick={this.handleSubmit}>Submit</Button>
-						{formSuccess === true && <span>Message Sent - Thank You</span>}
-						{formSuccess === false && <span> Please Double Check Your Information</span>}
+						<Button color='blue' disabled={disableSubmit} onClick={this.handleSubmit}>Submit</Button>
+						{/* {formSuccess === true && <span>Message Sent - Thank You</span>}
+						{formSuccess === false && <span> Please Double Check Your Information</span>} */}
 					</Form>
 				</Modal.Content>
-			</Modal>
+			</Modal >
 		)
 	}
 }
