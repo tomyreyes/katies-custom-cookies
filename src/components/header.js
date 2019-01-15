7c29738e0fbc68630765820ede620b9e3861cfa6
Tomy <tomyleereyes@gmail.com>
2019-01-15 12:58:07 -0800

    test build

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,8 @@ import logo from '../assets/images/katieslogo.png'
 const Header = props => (
   <header id="header" className="alt">
     <Link to="/" className="logo">
-      <img src={logo} />
+      {/* <img src={logo} /> */}
+      Katies Custom Cookies
     </Link>
     <nav>
       <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">
